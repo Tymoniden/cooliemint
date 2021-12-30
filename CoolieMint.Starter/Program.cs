f93d4c64dd5a22770b14f80aac99d9e9e0845213
Tymoniden <timo.steib@gmail.com>
2021-12-30 13:54:31 +0100

    Add starter project to solution

---
+++ CoolieMint.Starter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+var appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+var directory = Directory.GetDirectories(appDirectory)
+    .Select(path => new DirectoryInfo(path))
+    .Where(d => d.Name.StartsWith("CoolieMint_"))
+    .OrderByDescending(d => d.Name)
+    .FirstOrDefault();
+
+if(directory == null)
+{
+    Console.WriteLine("No directory found.");
+    return;
+}
+
+var targetFile = directory.GetFiles().FirstOrDefault(f => f.Name.Equals("WebControlCenter"));
+if(targetFile == null)
+{
+    Console.WriteLine("Target file not found.");
+    return;
+}
+
+var processStartInfo = new ProcessStartInfo
+{
+    FileName = targetFile.FullName,
+    WorkingDirectory = targetFile.DirectoryName
+};
+
+Console.WriteLine($"Starting file: {processStartInfo.FileName}");
+var process = Process.Start(processStartInfo);
+process?.WaitForExit();
+
+Console.WriteLine($"{processStartInfo.FileName} exited with code: {process.ExitCode}");
+Environment.Exit(process?.ExitCode ?? 0);
